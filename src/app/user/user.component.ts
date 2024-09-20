e5be44d11454eb73f53f7e61580bddf7ede2ae26
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-20 10:15:11 +0200

    storing data models in separate files

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import { Component, Input, output } from '@angular/core';
-
-// type User = {
-//   id:string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User{
-  id:string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
---
+++ src/app/user/user.model.ts
@@ -0,0 +1,13 @@
+export interface User{
+    id:string;
+    avatar: string;
+    name: string;
+  }
+
+// Same syntax
+// type User = {
+//   id:string;
+//   avatar: string;
+//   name: string;
+// }
+
